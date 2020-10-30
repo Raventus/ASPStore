52ac28648cab13e43c27af42771ad0d3fc65ccec
Андрей Колдырев <raven4791f@mail.ru>
2020-10-30 17:03:21 +0700

    The infrastructure of the project is created

---
+++ SportStore/Controlles/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+
+namespace SportStore.Controlles
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+        public ProductController (IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult List() => View(_repository.Products);
+    }
+}
